bcc1ce7158838018ef2ebc3b336d28c44724f909
Николай Чернявский <chern.nic@gmail.com>
2023-05-03 10:45:36 +0300

    first commit

---
+++ PiskaPerepiska/MVVM/View/HostWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using PiskaPerepiska.MVVM.Model;
+using PiskaPerepiska.MVVM.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PiskaPerepiska.MVVM.View
+{
+    /// <summary>
+    /// Логика взаимодействия для HostWindow.xaml
+    /// </summary>
+    public partial class HostWindow : Window
+    {
+        public HostWindow()
+        {
+            InitializeComponent();
+            DataContext = new HostVM();
+        }
+    }
+}
